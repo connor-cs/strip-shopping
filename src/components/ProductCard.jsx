1d1ed7725d3767226d5788a9b0fa5098d8df010d
Connor <connor.cs@protonmail.com>
2023-08-16 20:57:50 -0400

    more

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,23 +2,36 @@ import { Card, Button, Form, Row, Col } from "react-bootstrap";
 import { CartContext } from "../CartContext";
 import { useContext } from "react";
 
-const ProductCard = ( {product} ) => {
+const ProductCard = ({ product }) => {
   const cart = useContext(CartContext);
-  // console.log(product);
-  // console.log(cart.items)
+  const quantity = cart.getProductQuantity(product.id);
   return (
     <Card>
       <Card.Body>
         <Card.Title>{product.name}</Card.Title>
         <Card.Text>${product.price}</Card.Text>
-        <Button
-          variant="primary"
-          onClick={() => {
-            cart.addOneToCart(product.id);
-          }}
-        >
-          Add to Cart
-        </Button>
+        {quantity > 0 ? (
+          <>
+            <Form as={Row}>
+              <Form.Label column="true" sm="6">
+                {quantity} in cart
+              </Form.Label>
+              <Row sm='4'>
+                <Button sm="6" onClick={()=>cart.addOneToCart(product.id)}className="mx-2">+</Button>
+                <Button sm="6" onClick={()=>cart.removeOneFromCart(product.id)}className="mx-2">-</Button>
+              </Row>
+            </Form>
+          </>
+        ) : (
+          <Button
+            variant="primary"
+            onClick={() => {
+              cart.addOneToCart(product.id);
+            }}
+          >
+            Add to Cart
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
